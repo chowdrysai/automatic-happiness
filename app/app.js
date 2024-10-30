5ee3b37e45a1dacf58dfc8b45a9903e1537595ef
chowdrysai <chowdrysai45@gmail.com>
2024-10-30 23:04:26 +0530

    changes

--- app/app.js
+++ app/app.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const serverless = require('serverless-http')
 const app = express()
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.get('/hello', (req, res) => {
+    res.send('deployed Successfully')
 })
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
 module.exports.handler = serverless(app)
